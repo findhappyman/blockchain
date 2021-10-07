c5979fb18eea2b77f44321f6a4827dbc4798ca40
Henry Xue <kidfullystar@gmail.com>
2021-10-07 16:34:22 +0800

    first commit

---
+++ brownie_simple_storage/tests/test_simple_storage.py
@@ -0,0 +1,21 @@
+from brownie import SimpleStorage, accounts
+
+
+def test_deploy():
+    # Arrange
+    account = accounts[0]
+    # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+
+    # Assert
+    assert starting_value == expected
+
+
+def test_updated_storage():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    expected = 15
+    simple_storage.store(15, {"from": account})
+    assert expected == simple_storage.retrieve()
